ce5b9c94c20abe1e2797b7294343db88385c82e2
Martin <martinconcetti97@gmail.com>
2024-08-26 14:29:39 -0300

    Text that shows speed

--- 2024-graficos/Assets/Scrips/Player/Movement Slider/MovementSlider.cs
+++ 2024-graficos/Assets/Scrips/Player/Movement Slider/MovementSlider.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MovementSlider : MonoBehaviour
 {
     [SerializeField] string player;
     [SerializeField] Slider mainSlider;
-
+    [SerializeField] TMP_Text text;
     void Awake()
     {
         Debug.Log("Value in slider is : " + PlayerPrefs.GetFloat(player));
 
         mainSlider.value = PlayerPrefs.GetFloat(player);
+        text.text = "player speed: " + PlayerPrefs.GetFloat(player).ToString();
     }
 
     public void Update()
     {
         PlayerPrefs.SetFloat(player, mainSlider.value);
+        text.text = "player speed: " + PlayerPrefs.GetFloat(player).ToString();
+
     }
 }
